c3f3c26bc80d73bfee6da1eb8a4efebc2939df49
BlueSkyXN <xienan0714@gmail.com>
2021-04-16 22:12:22 +0800

    Update lovespeed.sh

--- lovespeed.sh
+++ lovespeed.sh
@@ -1,5 +1,5 @@
 rm -rf "/tmp/speed"
-"/root/speedtest" -s 17129  >  "/tmp/speed"
+"/root/speedtest"  >  "/tmp/speed"
 ls1=`cat /tmp/speed | grep Upload | awk '{gsub(/^\s+|\s+$/, "");print}'`
 ls2=`cat /tmp/speed | grep Download | awk '{gsub(/^\s+|\s+$/, "");print}'`
 lsn=hostname
