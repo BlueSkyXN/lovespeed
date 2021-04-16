afcc3c190930cfebfdb49ac517a0550e289f5f58
BlueSkyXN <xienan0714@gmail.com>
2021-04-16 22:12:04 +0800

    Update lovespeed.sh

--- lovespeed.sh
+++ lovespeed.sh
@@ -1,9 +1,11 @@
 rm -rf "/tmp/speed"
-"/root/speedtest"  >  "/tmp/speed"
-ls1=cat /tmp/speed | grep Upload | awk '{gsub(/^\s+|\s+$/, "");print}'
-ls2=cat /tmp/speed | grep Download | awk '{gsub(/^\s+|\s+$/, "");print}'
+"/root/speedtest" -s 17129  >  "/tmp/speed"
+ls1=`cat /tmp/speed | grep Upload | awk '{gsub(/^\s+|\s+$/, "");print}'`
+ls2=`cat /tmp/speed | grep Download | awk '{gsub(/^\s+|\s+$/, "");print}'`
 lsn=hostname
-curl --data chat_id="『聊天ID』" --data-urlencode "text=§$lsn§
+tgid=
+tgbotapi=
+curl --data chat_id="$tgid" --data-urlencode "text=§$lsn§
 
 $ls1
-$ls2" "https://api.telegram.org/bot『API』/sendMessage?parse_mode=HTML"
+$ls2" "https://api.telegram.org/bot$tgbotapi/sendMessage?parse_mode=HTML"
