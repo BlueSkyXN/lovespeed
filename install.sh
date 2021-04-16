dbee8c3ebd416337795d882e2162144e2d22ccfa
BlueSkyXN <xienan0714@gmail.com>
2021-04-16 22:35:49 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo wget -O "/root/speedtest" "https://raw.githubusercontent.com/BlueSkyXN/ChangeSource/master/speedtest" --no-check-certificate -T 30 -t 5 -d
+sudo chmod +x "/root/speedtest"
+sudo chmod 777 "/root/speedtest"
+sudo wget -O "/root/lovespeed.sh" "https://raw.githubusercontent.com/BlueSkyXN/lovespeed/main/lovespeed.sh" --no-check-certificate -T 30 -t 5 -d
+sudo chmod +x "/root/lovespeed.sh"
+sudo chmod 777 "/root/lovespeed.sh"
